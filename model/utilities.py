08e5820add56cb5da06f99f35405706c9e8c31a1
essea <yaroslav.tsepkov@gmail.com>
2021-11-11 22:22:37 +0400

    update iou

--- model/utilities.py
+++ model/utilities.py
@@ -10,7 +10,7 @@ def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
     Returns:
         tensor: Intersection over union for all examples
     """
-    
+
     if box_format == "midpoint":
         box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
         box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
@@ -36,7 +36,6 @@ def intersection_over_union(boxes_preds, boxes_labels, box_format="midpoint"):
     x2 = torch.min(box1_x2, box2_x2)
     y2 = torch.min(box1_y2, box2_y2)
 
-    # Need clamp(0) in case they do not intersect, then we want intersection to be 0
     intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
     box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
     box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
