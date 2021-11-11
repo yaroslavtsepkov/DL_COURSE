3e2dce1e916030101f6e0caceb78ea5dff02d8e6
essea <yaroslav.tsepkov@gmail.com>
2021-11-11 22:21:06 +0400

    add creater csv

--- data/support_data.py
+++ data/support_data.py
@@ -2,6 +2,8 @@ from torchvision.transforms.functional import adjust_sharpness
 from torchvision.utils import draw_bounding_boxes
 from torchvision import transforms as T
 import torch
+import pandas as pd
+import os
 
 def show_bbox(img:torch.Tensor, bbox:torch.Tensor, **kwargs):
     bbox = converter(bbox)
@@ -25,6 +27,32 @@ def converter(bbox: torch.Tensor)->torch.Tensor:
     new_bbox[:, :2] = bbox[:, :2]
     new_bbox[:, 2:] = bbox[:, :2] + bbox[:, 2:]
     return new_bbox
+
+def createCSV(split="train", output="data/dataset"):
+    annot = {
+        "filename":[],
+        "bboxes":[]
+    }
+    with open(f"/home/essea/repos/Yolo/data/wider_face_split/wider_face_{split}_bbx_gt.txt","r") as f:
+        while True:
+            name = f.readline().rstrip()
+            if not name:
+                break
+
+            try:
+                numfaces = int(f.readline().rstrip())
+            except:
+                continue
+            temp = []
+            for _ in range(numfaces):
+                row = list(map(int, f.readline().rstrip().split()[:4]))
+                temp.append(row)
+            annot['filename'].append(name)
+            annot['bboxes'].append(temp)
+    df = pd.DataFrame.from_dict(annot)
+    df["filename"] = df["filename"].map(lambda x: os.path.join(f"/home/essea/datasets/WIDER_{split}/",x))
+    output = os.path.join(output, f"{split}.csv")
+    df.to_csv(output)
 
 if __name__ == '__main__':
     pass
